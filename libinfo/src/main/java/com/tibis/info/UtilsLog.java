bc00639bea2a27f6f038c45e31d7a7866aba4710
osboxes.org <lucky@k5planet.com>
2019-11-20 15:01:41 +0700

    update logger

--- libinfo/src/main/java/com/tibis/info/UtilsLog.java
+++ libinfo/src/main/java/com/tibis/info/UtilsLog.java
@@ -1,10 +1,22 @@
 package com.tibis.info;
 
 import android.util.Log;
+import com.android.logx.Ulog;
 
 public class UtilsLog {
-    public static void Debug(String msg)
+    public static void debug(String msg)
     {
         Log.d("TAG",msg);
     }
+
+    public  static void info (String msg)
+    {
+        Ulog.info(msg);
+    }
+
+    public  static void error (String msg)
+    {
+        Ulog.debug(msg);
+    }
+
 }
